dc964ba74cc2997057880396d3c5ae8f7443fa0e
wangzhaoxing <wangzhaoxing@meituan.com>
2025-06-16 10:55:10 +0800

    🔧 最终修复: 解决Favicon 404错误和潜在的JS语法问题

--- api/index.py
+++ api/index.py
@@ -1896,5 +1896,18 @@ def export_csv(data):
 # Vercel需要的应用实例
 application = app
 
+@app.route('/favicon.ico')
+def favicon():
+    """提供网站图标"""
+    # 返回一个简单的SVG作为图标，避免404错误
+    return Response(
+        (
+            b'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">'
+            b'<path fill="%2306D6A0" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>'
+            b'</svg>'
+        ),
+        mimetype='image/svg+xml'
+    )
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8050) 
